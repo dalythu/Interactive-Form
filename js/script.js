712c53ef0483f31769f7b1ebb28c7e4e19664f41
Thurston Daly <dalythu@gmail.com>
2021-07-13 01:17:43 -0400

    changed event listener to use color instead of theme

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ jobRoleField.addEventListener("change", e => {
         otherJobRoleField.style.display = 'none';
     }
 });
+
+const design = document.getElementById("design");
+const color = document.getElementById("color");
+const colorOptions = color.children;
+
+color.disabled = true;
+
+design.addEventListener("change", e => {
+    color.disabled = false;
+    for (let i=0; i<colorOptions.length;i++) {
+        const currentOption = colorOptions[i];
+        const dataTheme = currentOption.getAttribute("data-theme");
+        if (dataTheme == design.value) {
+            currentOption.selected = true;
+            currentOption.hidden = false;
+        } else {
+            currentOption.selected = false;
+            currentOption.hidden = true;
+        }
+    }
+});
