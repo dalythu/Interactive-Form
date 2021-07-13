835b5529a2486650f3d0cb01982de4c53f990ce2
Thurston Daly <dalythu@gmail.com>
2021-07-13 00:19:12 -0400

    Update script.js
    added job role/other job role functionality using an if statement

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-document.getElementById("name").focus();
+const nameField = document.getElementById("name")
+const jobRoleField = document.getElementById("title")
+const otherJobRoleField = document.getElementById("other-job-role")
+
+nameField.focus();
+
+otherJobRoleField.style.display = 'none';
+
+jobRoleField.addEventListener("change", e => {
+    if (e.target.value == "other") {
+        otherJobRoleField.style.display = 'inherit';
+    } else {
+        otherJobRoleField.style.display = 'none';
+    }
+});
